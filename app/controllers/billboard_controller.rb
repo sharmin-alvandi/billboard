14f588fbb55c7cbd89af3d1804f30ccccb7b2753
Sharmin Alvandi <sharmin.alvandi@shopify.com>
2019-11-28 16:19:22 -0500

    complete search on collections

--- app/controllers/billboard_controller.rb
+++ app/controllers/billboard_controller.rb
@@ -2,5 +2,21 @@ class BillboardController < ApplicationController
   def index
     @collections = Collection.all
     @advertisements = Advertisement.all
+    if params[:collection_id] 
+      @collection_id = params[:collection_id]
+      @advertisements = Advertisement.where("collection_id = ?", @collection_id)
+      @advertisements = Advertisement.all if params[:collection_id] == ""
+    end
+    
   end
+
+  def search
+    @collection_id = params[:collection_id]
+    @advertisements = Advertisement.where("collection_id = ?", @collection_id)
+  end
+
+  # def advertisement_params
+  #   params.require(:advertisement).permit(:title, :store_id, :product_url, :description, :image_url, :collection_id)
+  # end
+
 end
