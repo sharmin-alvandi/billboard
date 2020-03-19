9c7465af1368fe61a171940ca84737de440be858
Sharmin Alvandi <sharmin.alvandi@shopify.com>
2020-03-19 15:45:39 -0400

    added error messages to the tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,7 @@ class UserTest < ActiveSupport::TestCase
   test "user can not be saved without a user name" do
     user = User.new(username: nil, password: "12345678")
     assert_not user.save
+    assert_equal(user.errors.messages, username: ["can't be blank"])
   end
 
   test "user name should be unique" do
@@ -11,10 +12,12 @@ class UserTest < ActiveSupport::TestCase
     user.save
     user = User.new(username: "testuser", password: "12345678")
     refute user.save
+    assert_equal(user.errors.messages, username: ["has already been taken"])
   end
 
   test "password length must be more than 5 characters" do
     user = User.new(username: "testuser", password: "1234")
     refute user.save
+    assert_equal(user.errors.messages, password: ["must be at least 5 characters"])
   end
 end
