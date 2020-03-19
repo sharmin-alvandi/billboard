ac8b35d3923eba584c2bc2880090305a1507eb33
Sharmin Alvandi <sharmin.alvandi@shopify.com>
2020-03-19 15:03:18 -0400

    tests some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  # validates_uniqueness_of :username
-  # validates_length_of :password, minimum: 5, too_short: 'must be at least 5 characters'
+  validates_uniqueness_of :username, case_sensitive: true
+  validates_length_of :password, minimum: 5, too_short: 'must be at least 5 characters'
 
   has_one :store
   has_secure_password
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,16 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(username: nil, password: "12345678")
     assert_not user.save
   end
+
+  test "user name should be unique" do
+    user = User.new(username: "testuser", password: "12345678")
+    user.save
+    user = User.new(username: "testuser", password: "12345678")
+    refute user.save
+  end
+
+  test "password length must be more than 5 characters" do
+    user = User.new(username: "testuser", password: "1234")
+    refute user.save
+  end
 end
