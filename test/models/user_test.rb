ea4effee3aecd39309c12f7bfc4c367d3eed32f0
Sharmin Alvandi <sharmin.alvandi@shopify.com>
2020-03-24 14:12:22 -0400

    Modified tests to add fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,22 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(username: 'testuser', password: '12345678')
+    # @user = users(:valid)
+  end
   test "user can not be saved without a user name" do
-    user = User.new(username: nil, password: "12345678")
-    assert_not user.save
-    assert_equal(user.errors.messages, username: ["can't be blank"])
+    @user.username = nil
+    refute @user.valid?, 'User saved without a user name'
+    refute_nil @user.errors[:username], 'no validation error for user name'
   end
 
   test "user name should be unique" do
-    user = User.new(username: "testuser", password: "12345678")
+    # user = User.new(username: "testuser", password: "12345678")
+    user = users(:valid)
     user.save
-    user = User.new(username: "testuser", password: "12345678")
+    user = users(:valid)
     refute user.save
     assert_equal(user.errors.messages, username: ["has already been taken"])
   end
 
   test "password length must not be less than 5 characters" do
-    user = User.new(username: "testuser", password: "1234")
+    # user = User.new(username: "testuser", password: "1234")
+    user = users(:invalid_password)
     refute user.save
     assert_includes user.errors.full_messages, "Password must be at least 5 characters"
   end
